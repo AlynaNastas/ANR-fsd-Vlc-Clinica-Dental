c08968e2edef92aeca67d4fea501c9249ca7c089
Alyna Nastas Romaniuc <nastasalyna@gmail.com>
2023-03-12 11:52:15 +0100

    fix:basic structure it was deleted

--- Clinica Dental/src/main.jsx
+++ Clinica Dental/src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { MainApp } from './mainapp/MainApp';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+    <BrowserRouter>
+    <MainApp/>
+    </BrowserRouter>
   </React.StrictMode>,
 )
---
+++ Clinica Dental/src/mainapp/Mainapp.jsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
+export const Mainapp = () => {
+  return (
+    <>
+    <Routes>
+
+    </Routes>
+    </>
+  )
+}
